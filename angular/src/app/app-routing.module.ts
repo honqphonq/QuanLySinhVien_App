dfd9610cf05e07048f4218e5a2260306ab4c45a5
honqphonq <le.hong.phong@gosei.com.vn>
2023-07-25 22:38:30 +0700

    remove unsed components.

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -5,9 +5,6 @@ import { AppRouteGuard } from '@shared/auth/auth-route-guard';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { UsersComponent } from './users/users.component';
-import { TenantsComponent } from './tenants/tenants.component';
-import { RolesComponent } from 'app/roles/roles.component';
-import { ChangePasswordComponent } from './users/change-password/change-password.component';
 
 @NgModule({
     imports: [
@@ -18,10 +15,7 @@ import { ChangePasswordComponent } from './users/change-password/change-password
                 children: [
                     { path: 'home', component: HomeComponent,  canActivate: [AppRouteGuard] },
                     { path: 'users', component: UsersComponent, data: { permission: 'Pages.Users' }, canActivate: [AppRouteGuard] },
-                    { path: 'roles', component: RolesComponent, data: { permission: 'Pages.Roles' }, canActivate: [AppRouteGuard] },
-                    { path: 'tenants', component: TenantsComponent, data: { permission: 'Pages.Tenants' }, canActivate: [AppRouteGuard] },
                     { path: 'about', component: AboutComponent, canActivate: [AppRouteGuard] },
-                    { path: 'update-password', component: ChangePasswordComponent, canActivate: [AppRouteGuard] }
                 ]
             }
         ])
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -14,20 +14,8 @@ import { ServiceProxyModule } from '@shared/service-proxies/service-proxy.module
 import { SharedModule } from '@shared/shared.module';
 import { HomeComponent } from '@app/home/home.component';
 import { AboutComponent } from '@app/about/about.component';
-// tenants
-import { TenantsComponent } from '@app/tenants/tenants.component';
-import { CreateTenantDialogComponent } from './tenants/create-tenant/create-tenant-dialog.component';
-import { EditTenantDialogComponent } from './tenants/edit-tenant/edit-tenant-dialog.component';
-// roles
-import { RolesComponent } from '@app/roles/roles.component';
-import { CreateRoleDialogComponent } from './roles/create-role/create-role-dialog.component';
-import { EditRoleDialogComponent } from './roles/edit-role/edit-role-dialog.component';
 // users
 import { UsersComponent } from '@app/users/users.component';
-import { CreateUserDialogComponent } from '@app/users/create-user/create-user-dialog.component';
-import { EditUserDialogComponent } from '@app/users/edit-user/edit-user-dialog.component';
-import { ChangePasswordComponent } from './users/change-password/change-password.component';
-import { ResetPasswordDialogComponent } from './users/reset-password/reset-password.component';
 // layout
 import { HeaderComponent } from './layout/header.component';
 import { HeaderLeftNavbarComponent } from './layout/header-left-navbar.component';
@@ -44,21 +32,7 @@ import { SidebarMenuComponent } from './layout/sidebar-menu.component';
         AppComponent,
         HomeComponent,
         AboutComponent,
-        // tenants
-        TenantsComponent,
-        CreateTenantDialogComponent,
-        EditTenantDialogComponent,
-        // roles
-        RolesComponent,
-        CreateRoleDialogComponent,
-        EditRoleDialogComponent,
-        // users
         UsersComponent,
-        CreateUserDialogComponent,
-        EditUserDialogComponent,
-        ChangePasswordComponent,
-        ResetPasswordDialogComponent,
-        // layout
         HeaderComponent,
         HeaderLeftNavbarComponent,
         HeaderLanguageMenuComponent,
